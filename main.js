c804418c7afb86f693f38ee0197079b75ea52c07
tjbyers18 <108040879+tjbyers18@users.noreply.github.com>
2022-06-23 20:29:32 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+// My Current Age
+var myAge = 34
+
+//Early Years
+var earlyYears = (2 * 10.5);
+
+// console.log (earlyYears);
+
+// Later Years
+var laterYears = (32 * 4);
+
+// console.log (laterYears);
+
+//My age in Dog Years
+
+var myAgeInDogYears = (earlyYears + laterYears);
+
+// console.log (myAgeInDogYears);
+
+// My Name
+
+var myName = 'TIMOTHY';
+// console.log (myName.toLowerCase);
+
+// Finishing Sentence
+
+console.log (`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
